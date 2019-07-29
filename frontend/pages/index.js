a8845a1e25d21b6b44b92d0e17a4192bad97f4f8
gilberto ayala <gilbertoayala12@gmail.com>
2019-07-29 12:09:03 -0700

    basics with next.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+const Home = props => (
+  <div>
+    <p>Heyoo </p>
+    <Link href="/sell">
+      <a>Vamo a sell</a>
+    </Link>
+  </div>
+);
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+const Sell = props => (
+  <div>
+    <p>Selling stuff </p>
+    <Link href="/">
+      <a>Vamo a casa</a>
+    </Link>
+  </div>
+);
+export default Sell;
